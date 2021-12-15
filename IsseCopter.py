aba88a771979712bbe28b90dbd1f6b40c52d0d93
Luca A <luca.alfano@student.uni-augsburg.de>
2021-12-15 15:07:10 +0100

    Added functionality

--- IsseCopter.py
+++ IsseCopter.py
@@ -1,22 +1,27 @@
 import signal
+import sys
 from threading import Thread
 from time import sleep
 
 from AbstractVirtualCapability import AbstractVirtualCapability, VirtualCapabilityServer
 
-
 class IsseCopter(AbstractVirtualCapability):
-
     def __init__(self, server):
         super().__init__(server)
+        self.ISSECopterPosition = [0., 0., 0.]
 
-    def execute_command(self, command: dict):
-        command["type"] = "respond"
-        self.send_message(command)
+    def SetISSECopterPosition(self, params: dict) -> dict:
+        return self.GetISSECopterPosition(params)
+
+    def GetISSECopterPosition(self, params: dict) -> dict:
+        return {"Position3D": self.ISSECopterPosition}
+
+    def FlyToPosition(self, params: dict) -> dict:
+        formatPrint(f"Flying to position {params}")
+        return self.SetISSECopterPosition(params)
 
     def loop(self):
-        sleep(5)
-        self.send_message({"alive":True})
+        pass
 
 
 if __name__ == "__main__":
@@ -27,7 +32,10 @@ if __name__ == "__main__":
         quit(1)
 
     try:
-        server = VirtualCapabilityServer()
+        port = None
+        if len(sys.argv[1:]) > 0:
+            port = int(sys.argv[1])
+        server = VirtualCapabilityServer(port)
         listener = IsseCopter(server)
         listener.start()
         signal.signal(signal.SIGTERM, handler)
@@ -35,4 +43,5 @@ if __name__ == "__main__":
         # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
     except KeyboardInterrupt:
         print("[Main] Received KeyboardInterrupt")
+        server.kill()
         listener.kill()
